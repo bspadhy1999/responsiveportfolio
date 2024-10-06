34a5ba64d326dd4a06900b8b4b03ad8a1e43ebd6
bspadhy1999 <bspadhy1999@gmail.com>
2024-10-06 17:47:50 +0530

    first commit

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ dark.onclick = function () {
     dark.src = "media/moon.png";
   }
 };
+
+function sendEmail() {
+  Email.send({
+    SecureToken : "19a35bb5-05d4-4fb4-93d6-87d7ff89da79",
+    To : 'https://bspadhy1999.github.io/myportfolio.github.io/',
+    From : document.getElementById("email").value,
+    Subject : "Message From Your Portfolio Website",
+    Body : "Name : " + document.getElementById("name").value +
+           "<br> Email : " + document.getElementById("email").value +
+           "<br> Mobile No. : " + document.getElementById("mobile").value +
+           "<br> Message : " + document.getElementById("message").value
+}).then(
+  message => swal("Thank u!", "You Message Sent Sucessfully.", "success")
+);
+}
